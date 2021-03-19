3a6e40c834067ecf1c3d00158f5946742d7af66c
Gezdaman <geraldbrent@outlook.com>
2021-03-19 02:06:23 +0000

    1st Commit

---
+++ App.py
@@ -0,0 +1,27 @@
+#Project Brainstorming
+#Now that you have an idea, visualize the end result:
+
+#What does your program do? TELLS CLAIMANT WHETHER TO BRING CLAIM IN CONTRACT OR TORT
+#How will it work in a terminal? YES
+#Is it one player or two players? ONE
+#Make sure that it satisfies all of the project objectives.
+
+#Make a timeline for yourself and avoid the temptation to build things that aren’t required. 
+#Setting firm boundaries and deadlines will keep you on track and prevent scope creep.
+
+#The following tasks will help you identify natural breakpoints.
+
+#Properly scoping your project will greatly benefit you; 
+#scoping creates structure while requiring you to think through your entire project before you begin. 
+#You should start with stating the goals for your project, then gathering the data, 
+#and considering the analytical steps required. 
+#A proper project scope can be a great road map for your project, 
+#but keep in mind that some down-stream tasks may become dead ends which will require adjustment to the scope.
+
+zdflmsdfv
+sdafgdsfgzdfgvdfsgzdfb
+zdfgzdfgbzdfbdzbf
+\xfv\sdf\sdgf\sorted
+\sdgv\sdg\sadfg\sdafgdsfgzdfgvdfsgzdfbsg
+assertvasgvasfveragbvadfgb asg
+vdfsgv\sgv\sfdgv\s
