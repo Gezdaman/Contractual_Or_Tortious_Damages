a3d275c450c1039db72d5f38909ff9b793ef3986
Gezdaman <63683507+Gezdaman@users.noreply.github.com>
2021-04-30 01:30:03 +0100

    Update (more accurate lawyering...)

--- App.py
+++ App.py
@@ -1,14 +1,17 @@
 x = input("What was the price of the good/service/business purchased?")
 print("The price of the good/service/business purchased was " + str(x))
 
-y = input("What was the represented value of the good/service/business purchased, according to the purchase contract?")
-print("The represented value of the good/service/business purchase was " + str(y))
+y = input("What was the warranted value of the good/service/business purchased, according to the purchase contract?")
+print("The warranted value of the good/service/business purchase was " + str(y))
 
 z = input("What is the actual value of the good/service/business purchased?")
 print("The actual value of the good/service/business purchased is " + str(z))
 
-if x >= z:
+if (x-z) > (y-x):
     print("You should claim on a tortious basis, pursuant to a pre-contractual misrepresentation(s).")
 
-if y >= z:
+if (y-x) > (x-z):
     print("You should claim on a contractual basis, pursuant to the representation or warranty made in the purchase contract.")
+
+elif (y-x) == (x-z):
+    print("Both the tortious measure and the contractual measure are equally advantageous.")
